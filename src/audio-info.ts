87ab89cd0ee25697c975c07c0afb1e1dd1ead795
Kenji Ginjo <120051456+KenjiGinjo@users.noreply.github.com>
2024-05-31 23:49:00 +0800

    feat: Add unit tests for audioInfo function

---
+++ src/audio-info.ts
@@ -0,0 +1,48 @@
+import { _spawn } from "./_base";
+import type { FfmpegAudioInfo } from "./types";
+import { extractError } from "./utils/extract-error";
+
+export const audioInfo = async (
+  filePath: string
+): Promise<FfmpegAudioInfo[]> => {
+  const proc = Bun.spawn(
+    [
+      "ffprobe",
+      "-v",
+      "error",
+      "-select_streams",
+      "a:0",
+      "-show_entries",
+      "stream=codec_name,channels,sample_rate,bit_rate",
+      "-of",
+      "json",
+      filePath,
+    ],
+    { stderr: "pipe" }
+  );
+
+  const exitCode = await proc.exited;
+
+  if (exitCode !== 0) {
+    const stderr = await Bun.readableStreamToText(proc.stderr);
+    const errors = extractError(stderr);
+    throw new Error(errors);
+  }
+
+  const stdout = (await new Response(proc.stdout).json()) as any;
+  const result = stdout?.streams as {
+    codec_name: string;
+    sample_rate: string;
+    channels: number;
+    bit_rate: string;
+  }[];
+
+  return result.map((r) => {
+    return {
+      codec: r.codec_name,
+      channels: r.channels,
+      sampleRate: parseInt(r.sample_rate),
+      bitrate: r.bit_rate,
+    };
+  });
+};
