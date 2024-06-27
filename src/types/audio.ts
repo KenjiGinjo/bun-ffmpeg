ab7b47fddb79d6570f8f6317591921afaec6424c
Kenji Ginjo <120051456+KenjiGinjo@users.noreply.github.com>
2024-06-27 21:57:12 +0800

    chore(types): update FfmpegAudioOptions interface to support custom codec, channels, and sample rate

--- src/types/audio.ts
+++ src/types/audio.ts
@@ -1,12 +1,12 @@
 export interface FfmpegAudioOptions {
-  codec?: 'aac' | 'mp3' | 'pcm_s16le';
+  codec?: 'aac' | 'mp3' | 'pcm_s16le' | string;
   /**
    * The bitrate for the audio encoding.
    * @example "192k"
    */
   bitrate?: string;
-  channels?: 1 | 2 | 5.1 | 7.1;
-  sampleRate?: 8000 | 16000 | 44100 | 48000;
+  channels?: 1 | 2 | 5.1 | 7.1 | number;
+  sampleRate?: 8000 | 16000 | 44100 | 48000 | number;
   quality?: number;
   onError?: (error: unknown) => void;
 }
