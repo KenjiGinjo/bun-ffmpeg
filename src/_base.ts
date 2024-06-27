d326ee8718652727c426049ebd180c8933109fa4
Kenji Ginjo <120051456+KenjiGinjo@users.noreply.github.com>
2024-06-27 21:57:47 +0800

    refactor: _base.ts for improved process handling

--- src/_base.ts
+++ src/_base.ts
@@ -52,16 +52,12 @@ export const _spawn = async (
     }
   };
 
-  try {
-    await Promise.all([processInput(), processOutput()]);
+  await Promise.all([processInput(), processOutput()]);
 
-    const exitCode = await proc.exited;
-    if (exitCode !== 0) {
-      const stderr = await Bun.readableStreamToText(proc.stderr);
-      const errors = extractError(stderr);
-      throw new Error(errors);
-    }
-  } catch (error) {
-    throw error;
+  const exitCode = await proc.exited;
+  if (exitCode !== 0) {
+    const stderr = await Bun.readableStreamToText(proc.stderr);
+    const errors = extractError(stderr);
+    throw new Error(errors);
   }
 };
